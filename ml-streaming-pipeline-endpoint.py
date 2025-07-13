e309dfd3ad1308723001b832162f843b6ef4f21e
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2025-07-13 14:58:47 +0530

    Adding Streaming job

--- ml-streaming-pipeline-endpoint.py
+++ ml-streaming-pipeline-endpoint.py
@@ -42,9 +42,38 @@ class Split(beam.DoFn):
             'Foreign_worker': str(Foreign_worker)
         }]
 def Filter_Data(data):
-    #This will remove rows the with Null values in any one of the columns
     return data['Purpose'] !=  'NULL' and len(data['Purpose']) <= 3  and  data['Property'] !=  'NULL' and data['Personal_status'] != 'NULL' and data['Existing_account'] != 'NULL' and data['Credit_amount'] != 'NULL' and data['Installment_plans'] != 'NULL'
 
+class ApplyLabelEncoding(beam.DoFn):
+    def __init__(self, bucket_name, encoder_folder, columns_to_encode):
+        self.bucket_name = bucket_name
+        self.encoder_folder = encoder_folder
+        self.columns_to_encode = columns_to_encode
+        self.encoders = {}
+
+    def setup(self):
+        # Load encoders only once per worker
+        from google.cloud import storage
+        import tempfile
+
+        client = storage.Client()
+        bucket = client.bucket(self.bucket_name)
+
+        for column in self.columns_to_encode:
+            blob_path = f"{self.encoder_folder}/{column}_label_encoder.pkl"
+            blob = bucket.blob(blob_path)
+
+            with tempfile.NamedTemporaryFile() as f:
+                blob.download_to_filename(f.name)
+                self.encoders[column] = joblib.load(f.name)
+
+    def process(self, element):
+        for col in self.columns_to_encode:
+            if col in element and element[col] in self.encoders[col].classes_:
+                element[col] = int(self.encoders[col].transform([element[col]])[0])
+            else:
+                element[col] = -1  # Unknown or unseen value
+        yield element
 def Convert_Datatype(data):
     #This will convert the datatype of columns from String to integers or Float values
     data['Duration_month'] = int(data['Duration_month']) if 'Duration_month' in data else None
@@ -56,47 +85,30 @@ def Convert_Datatype(data):
     data['Liable_People'] = int(data['Liable_People']) if 'Liable_People' in data else None
     return data
 
-def call_vertex_ai(data):
-    aiplatform.init(project='827249641444', location='asia-south1')
-    feature_order = ['Existing_account', 'Duration_month', 'Credit_history', 'Purpose',
-                 'Credit_amount', 'Saving', 'Employment_duration', 'Installment_rate',
-                 'Personal_status', 'Debtors', 'Residential_Duration', 'Property', 'Age',
-                 'Installment_plans', 'Housing', 'Number_of_credits', 'Job', 
-                 'Liable_People', 'Telephone', 'Foreign_worker']
-    endpoint = aiplatform.Endpoint(endpoint_name=f"projects/827249641444/locations/asia-south1/endpoints/6457541741091225600")
-    features = [data[feature] for feature in feature_order]
-    response = endpoint.predict(
-        instances=[features]
-    )
-    
-    prediction = response.predictions[0]
-    data['Classification'] = int(prediction)
-    return data
 
-# def call_vertex_ai(data):
-#     aiplatform.init(project='827249641444', location='asia-south1')
-#     endpoints = aiplatform.Endpoint.list()
-#     # Display endpoint info
-#     for endpoint in endpoints:
-#         print(f"Resource Name: {endpoint.resource_name}")
-#         endpoint_created = endpoint.resource_name
-    
-#     feature_order = ['Existing_account', 'Duration_month', 'Credit_history', 'Purpose',
-#                  'Credit_amount', 'Saving', 'Employment_duration', 'Installment_rate',
-#                  'Personal_status', 'Debtors', 'Residential_Duration', 'Property', 'Age',
-#                  'Installment_plans', 'Housing', 'Number_of_credits', 'Job', 
-#                  'Liable_People', 'Telephone', 'Foreign_worker']
-#     #endpoint = aiplatform.Endpoint(endpoint_name=f"projects/827249641444/locations/asia-south1/endpoints/6457541741091225600")
-#     endpoint = aiplatform.Endpoint(endpoint_name=endpoint_created)
-#     features = [data[feature] for feature in feature_order]
-#     response = endpoint.predict(
-#         instances=[features]
-#     )
-    
-#     prediction = response.predictions[0]
-#     data['Classification'] = int(prediction)
-#     return data
-    
+def call_vertex_ai(data):
+     aiplatform.init(project='827249641444', location='asia-south1')
+     endpoints = aiplatform.Endpoint.list()
+     # Display endpoint info
+     for endpoint in endpoints:
+         print(f"Resource Name: {endpoint.resource_name}")
+         endpoint_created = endpoint.resource_name
+   
+     feature_order = ['Existing_account', 'Duration_month', 'Credit_history', 'Purpose',
+                  'Credit_amount', 'Saving', 'Employment_duration', 'Installment_rate',
+                  'Personal_status', 'Debtors', 'Residential_Duration', 'Property', 'Age',
+                  'Installment_plans', 'Housing', 'Number_of_credits', 'Job', 
+                  'Liable_People', 'Telephone', 'Foreign_worker']
+     endpoint = aiplatform.Endpoint(endpoint_name=endpoint_created)
+     features = [data[feature] for feature in feature_order]
+     response = endpoint.predict(
+         instances=[features]
+     )
+   
+     prediction = response.predictions[0]
+     data['Classification'] = int(prediction)
+     return data
+   
 def run(argv=None, save_main_session=True):
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -120,6 +132,11 @@ def run(argv=None, save_main_session=True):
     PROJECT_ID = known_args.project
     TOPIC = known_args.input_topic
     SUBSCRIPTION = known_args.input_subscription
+    categorical_columns = [
+    'Existing_account', 'Credit_history', 'Purpose', 'Saving',
+    'Employment_duration', 'Personal_status', 'Debtors', 'Property',
+    'Installment_plans', 'Housing', 'Job', 'Telephone', 'Foreign_worker'
+    ]
     with beam.Pipeline(options=PipelineOptions()) as p:
         Encoded_data   = (p 
                        | 'Read data' >> beam.io.ReadFromPubSub(topic=TOPIC).with_output_types(bytes) )
@@ -129,13 +146,20 @@ def run(argv=None, save_main_session=True):
                        | 'Parsing Data' >> beam.ParDo(Split()))
         filtered_data = (Parsed_data
                      | 'Filtering Data' >> beam.Filter(Filter_Data))
-        Converted_data = (filtered_data
+        Encoded_data = (filtered_data 
+                    | 'Label Encoding' >> beam.ParDo(ApplyLabelEncoding(
+                        bucket_name=known_args.bucket_name,
+                        encoder_folder="label_encoders",
+                        columns_to_encode=categorical_columns
+                    ))
+                )
+        Converted_data = (Encoded_data
                        | 'Convert Datatypes' >> beam.Map(Convert_Datatype))
         Prediction   = (Converted_data
                         |'Get Inference' >> beam.Map(call_vertex_ai))
         output         = ( Prediction      
                        | 'Writing to bigquery' >> beam.io.WriteToBigQuery(
-                       '{0}:GermanCredit.GermanCreditTable-streaming'.format(PROJECT_ID),
+                       '{0}:GermanCredit.GermanCreditTable'.format(PROJECT_ID),
                        schema=SCHEMA,
                        write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
                       )
